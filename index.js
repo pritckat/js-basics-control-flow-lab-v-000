c9a87eaba5115db4a872e8747dcfc3ce1757db05
Katrina Pritchard <pritckat@gmail.com>
2018-10-10 23:45:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 // Write your code in this file!
-function scuberGreetingForFeet() {
+function scuberGreetingForFeet(someValue) {
+
+  let result
+  if (someValue =< 400) {
+    result = 'This one is on me!'
+  }
   
 }
