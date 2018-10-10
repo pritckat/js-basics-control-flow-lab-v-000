da398da6c1b76a9d8a04695994f84ea135750782
Katrina Pritchard <pritckat@gmail.com>
2018-10-10 23:49:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ function scuberGreetingForFeet(someValue) {
     result = 'This one is on me!'
   }
 
+  else if (someValue > 2500) {
+    result = 'No can do.'
+  }
+
   else if (someValue >2000) {
     result = 'I will gladly take your thirty bucks.'
   }
